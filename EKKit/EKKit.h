569cb0656406c34b788dd00f8ea3f392bd03c253
Yanis SIDAHMED <yanis.sidahmed@ekino.com>
2017-05-29 11:25:47 +0200

    Create EKKit.framework

---
+++ EKKit/EKKit.h
@@ -0,0 +1,19 @@
+//
+//  EKKit.h
+//  EKKit
+//
+//  Created by Yanis Sidahmed on 29/05/2017.
+//  Copyright © 2017 Ekino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EKKit.
+FOUNDATION_EXPORT double EKKitVersionNumber;
+
+//! Project version string for EKKit.
+FOUNDATION_EXPORT const unsigned char EKKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EKKit/PublicHeader.h>
+
+
